cde299dccc7c084de92970fb708e19cd5c5fb807
Sulton03 <131888493+Sulton03@users.noreply.github.com>
2023-12-19 03:58:43 +0500

    add files

---
+++ src/components/Resume/index.js
@@ -0,0 +1,135 @@
+import BarGraph from "./BarGraph";
+import DownloadIcon from "@mui/icons-material/Download";
+
+export default function Resume() {
+  return (
+    <div id="resume" className="min-h-screen flex-col text-center">
+      <div className="text-5xl font-bold py-8" data-aos={"slide-down"}>
+        Resume
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-7 mt-7">
+        <ol class="relative border-l  border-gray-700 ml-9 mb-6">
+          <li class="mb-10" data-aos="zoom-in">
+            <span class="absolute flex items-center justify-center w-6 h-6   rounded-full -left-3 ring-8  ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3  text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <h3 class="flex items-center mb-1 text-lg font-semibold  text-white ml-28">
+            Student of the National university of Uzbekistan
+            </h3>
+            <time class="block pt-1 pb-3  text-sm font-normal leading-none  text-gray-500">
+            years of study 2021-2025
+            </time>
+            <p class="mb-4 text-base font-normal  text-gray-400">
+            "I'm a third-year student at the Faculty of Mathematics and Intelligent Technologies, 
+            specializing in Computer Science and Programming Technologies
+            </p>
+            <a
+              href="https://drive.google.com/file/d/1__NeT89C3sLwCOserfoKq8R1RgdfquCd/view?usp=sharing"
+              target="_blank"
+              rel="noreferrer"
+              class="inline-flex items-center px-4 py-2 mt-2 text-sm font-medium border rounded-lg  focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+            >
+              <svg
+                class="w-4 h-4 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>{" "}
+              Download Certificate
+            </a>
+          </li>
+          <li class="mb-10" data-aos="zoom-out" data-aos-delay="700">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3 text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+           
+            <h3 class="mb-1 text-lg font-semibold text-white">
+            front-end developer course from IT-Park Tashkent 
+            </h3>
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-500">
+              Completed on November 17, 2023
+            </time>
+            <p class="text-base font-normal text-gray-400">
+              Learns how to style your site quickly with Bootstrap. I also learn
+              how to add logic to your CSS styles.
+              Later, I also built  other applications to
+              learn how to create powerful Single Page Applications with
+              React.
+            </p>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://drive.google.com/file/d/1vtI0dRQ6Ea-wPxwzw0tsHfyE6FcgQM50/view?usp=sharing"
+              class="inline-flex items-center px-4 py-2 mt-5 text-sm font-medium border rounded-lg focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+            >
+              <svg
+                class="w-4 h-4 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>{" "}
+              Download Certificate
+            </a>
+          </li>
+        </ol>
+
+        <div className="flex flex-col text-center" data-aos="slide-up">
+          <div className="text-3xl font-bold py-3">My Skills</div>
+          <div className="ml-16 flex flex-col gap-8 mt-6">
+            <BarGraph name={"JavaScript"} percent={65} />
+            <BarGraph name={"React"} percent={75} />
+           
+           
+            <BarGraph name={"HTML/CSS"} percent={85} />
+            <BarGraph name={"SQL"} percent={65} />
+            <BarGraph name={"GitHub"} percent={60} />
+          </div>
+
+          <a
+            href={require("../../assets/files/Sulton_resume.pdf")}
+            download={"Sulton's Resume"}
+            class="inline-flex items-center px-6 py-2 mt-12 mx-auto w-fit text-sm font-medium border rounded-lg  focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+          >
+            Download CV <DownloadIcon />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/constants/index.js
@@ -0,0 +1,120 @@
+export const projects = [
+  {
+    title: "Polar Ice-cream",
+    
+    tags: [
+      {
+        name: "Html",
+        color: "red-600",
+      },
+      {
+        name: "Css",
+        color: "green-600",
+      },
+      {
+        name: "bootstrap",
+        color: "blue-600",
+      },
+    ],
+    img: "https://pbs.twimg.com/profile_images/708528481747898368/kvLvFYcv_400x400.jpg",
+    
+    code: "https://github.com/sulton2003/Polar-Ice-cream",
+  },
+  {
+    title: "BURGER",
+   
+    tags: [
+      {
+        name: "Html",
+        color: "red-600",
+      },
+      {
+        name: "Css",
+        color: "green-600",
+      },
+      {
+        name: "bootstrap",
+        color: "blue-600",
+      },
+    ],
+    img: "https://mir-s3-cdn-cf.behance.net/projects/404/d86d16165662181.Y3JvcCwxNjM2LDEyODAsNDgsMA.jpg",
+    link: "https://project-menu-snowy.vercel.app",
+    code: "https://github.com/sulton2003/BURGER",
+  },
+  {
+    title: "Online-store",
+    
+    tags: [
+      {
+        name: "Html",
+        color: "red-600",
+      },
+      {
+        name: "Css",
+        color: "green-600",
+      },
+      {
+        name: "bootstrap",
+        color: "blue-600",
+      },
+      {
+        name: "javascript",
+        color: "yellow-600",
+      },
+    ],
+    img: "https://png.pngtree.com/png-vector/20200408/ourmid/pngtree-online-shopping-png-image_2179083.jpg",
+    
+    code: "https://github.com/Sulton03/online-store",
+  },
+  // {
+  //   title: "Stock Market",
+  //   desc: "Stock Market Website done with ReactJs as Frontend and Express as Backend. This websites gives practice for people those who want to start using stock market. It is based on realtime update of stock value.",
+  //   tags: [
+  //     {
+  //       name: "ReactJs",
+  //       color: "red-600",
+  //     },
+  //     {
+  //       name: "ExpressJs",
+  //       color: "green-600",
+  //     },
+  //     {
+  //       name: "MongoDB",
+  //       color: "blue-800",
+  //     },
+  //   ],
+  //   img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109032447783469097/image.png",
+  //   link: "https://stock-market-seven.vercel.app/",
+  //   code: "https://github.com/daniel-jebarson/Stock-Market",
+  // },
+  // {
+  //   title: "You-Farm",
+  //   desc: "An app made with NextJs, Machine Learning and ExpressJs to help farmers to sell their crops without intermediate. It also suggests which crops are needed for the specific soil.",
+  //   tags: [
+  //     {
+  //       name: "NextJs",
+  //       color: "red-600",
+  //     },
+  //     {
+  //       name: "MaterialUI",
+  //       color: "green-600",
+  //     },
+  //     {
+  //       name: "ExpressJs",
+  //       color: "yellow-600",
+  //     },
+  //     {
+  //       name: "FireBase",
+  //       color: "blue-800",
+  //     },
+  //     {
+  //       name: "Machine Learning",
+  //       color: "orange-600",
+  //     },
+  //   ],
+  //   img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109035277814611978/Screenshot_19.png",
+  //   link: "https://github.com/code-cracked/you-farm",
+  //   code: "https://github.com/code-cracked/you-farm",
+  // }
+  ,
+];
